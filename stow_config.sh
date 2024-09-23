6d234a30640f3970913900aa5ec72934393d013a
tihawk <cccp.hawk@gmail.com>
2024-09-23 17:51:53 +0300

    migrated to stow config structure

---
+++ stow_config.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define an array of package names
+packages=(
+  "bash"
+  "gtk-3.0"
+  "i3"
+  "i3blocks"
+  "terminator"
+  "nvim"
+)
+
+# Check if the first argument is "remove" to use the -D flag
+stow_flag="-t"
+if [ "$1" == "remove" ]; then
+  stow_flag="-D -t"
+fi
+
+# Loop through each package and run stow or unstow with -D
+for package in "${packages[@]}"; do
+  if [ "$1" == "remove" ]; then
+    echo "Unstowing $package..."
+  else
+    echo "Stowing $package..."
+  fi
+
+  stow $stow_flag "$HOME" "$package"
+
+  if [ $? -eq 0 ]; then
+    if [ "$1" == "remove" ]; then
+      echo "$package unstowed successfully."
+    else
+      echo "$package stowed successfully."
+    fi
+  else
+    if [ "$1" == "remove" ]; then
+      echo "Error unstowing $package."
+    else
+      echo "Error stowing $package."
+    fi
+  fi
+done
+
+echo "All done!"
+
