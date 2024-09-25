f7f01b380a9aa886f6940c8d0d7d85ead2e28d33
tihawk <cccp.hawk@gmail.com>
2024-09-25 22:27:25 +0300

    updated

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,25 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- Ripgrep await for input to search for
 vim.keymap.set('n', '<leader>ps', function()
-	-- This requires the ripgrep binary to be installed
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  -- This requires the ripgrep binary to be installed
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- Ripgrep for <cword> (word under cursor)
+vim.keymap.set('n', '<leader>pws', function()
+  local word = vim.fn.expand("<cword>")
+  builtin.grep_string({ search = word })
+end)
+
+-- Ripgrep for <cWORD> (sentence under cursor)
+vim.keymap.set('n', '<leader>pWs', function()
+  local word = vim.fn.expand("<cWORD>")
+  builtin.grep_string({ search = word })
+end)
+
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
--- nvim/.config/nvim/lua/kiroki/remap.lua
+++ nvim/.config/nvim/lua/kiroki/remap.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
+
+-- Map <C-c> to <Esc>, because LSP checks happen on escape, but not on ctrl-c
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Find and replace macro for word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
