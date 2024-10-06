5264a76dd2c5b65bc4ee281bd5ebb4d91c603ca5
tihawk <cccp.hawk@gmail.com>
2024-10-06 19:42:35 +0300

    updated

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,17 @@
 vim.opt_local.conceallevel = 1
+vim.opt_local.linebreak = true
 vim.opt_local.textwidth = 80
+
+-- Switch wrapping of markdown buffer between soft and hard
+---@param softOrHard string|?
+function SwitchWrapping(softOrHard)
+  softOrHard = softOrHard or "hard"
+
+  if (softOrHard == "hard") then
+    vim.opt_local.textwidth = 80
+    vim.opt_local.wrap = false
+  elseif softOrHard == "soft" then
+    vim.opt_local.textwidth = 0
+    vim.opt_local.wrap = true
+  end
+end
